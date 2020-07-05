5972a9cdfd65e9824bba87c3fc8e5efaf1d19341
Sneha Mishra <33183263+Tlazypanda@users.noreply.github.com>
2020-07-05 14:25:21 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'passenger start',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
